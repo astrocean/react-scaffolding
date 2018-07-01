f36091f4c3cf4bba80dbc44310e35fca3c90c21e
astrocean <rftstars@qq.com>
2018-07-01 15:39:13 +0800

    add react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const render=(Component)=>{
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+render(App);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+if(module.hot){
+  module.hot.accept('./App',()=>{
+    const App=require('./App').default;
+    render(App);
+  });
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
